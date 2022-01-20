f34c4bdb9623e57b15267f3b316c6b4276b8ee63
KimEui <kainor@naver.com>
2022-01-20 19:41:42 +0900

    데이터소스를 서버쪽에서 생성하고, 애플리케이션 쪽에서는 참조만 하도록 수정

--- webPro/src/spms/listeners/ContextLoaderListener.java
+++ webPro/src/spms/listeners/ContextLoaderListener.java
@@ -1,30 +1,23 @@
 package spms.listeners;
 
-import java.sql.SQLException;
-
+import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-
-import org.apache.commons.dbcp.BasicDataSource;
+import javax.sql.DataSource;
 
 import spms.dao.MemberDao;
 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener{
-	BasicDataSource ds;
-	
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		try {
 			ServletContext sc = event.getServletContext();
 			
-			ds = new BasicDataSource();
-			ds.setDriverClassName(sc.getInitParameter("driver"));
-			ds.setUrl(sc.getInitParameter("url"));
-			ds.setUsername(sc.getInitParameter("username"));
-			ds.setPassword(sc.getInitParameter("password"));
+			InitialContext initialContext = new InitialContext();
+			DataSource ds = (DataSource)initialContext.lookup("java:comp/env/jdbc/studydb");
 			
 			MemberDao memberDao = new MemberDao();
 			memberDao.setDataSource(ds);
@@ -38,8 +31,6 @@ public class ContextLoaderListener implements ServletContextListener{
 	}
 	
 	@Override
-	public void contextDestroyed(ServletContextEvent event) {
-		try { if (ds != null) ds.close(); } catch (SQLException e) {}
-	}
+	public void contextDestroyed(ServletContextEvent event) {}
 
 }
